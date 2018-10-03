5332cbe671122e7ca9084e132b2b0547b273e53a
Vitaliy <vitaliy.cva.socio@gmail.com>
2018-10-03 11:48:23 +0600

    Kth Symbol In Grammar second trial

--- JS/KthSymbolInGrammar/kthSymbolInGrammar.js
+++ JS/KthSymbolInGrammar/kthSymbolInGrammar.js
@@ -10,24 +10,54 @@ row 3: 0110
 row 4: 01101001
 */
 function generateRow(N) {
-    function iteration(N, prevRow) {
-        if (N > 1 ) {
-            let valuesToReplace = prevRow.match(new RegExp('[0, 1]', 'g'));
-            let nextRow = '';
-            valuesToReplace.forEach(function(value) {
-                let replacedValue = value === '0' ? '01' : '10';
-                nextRow += replacedValue;
-            });
-            return iteration(N-1, nextRow);
+    let valuesToReplace, nextRow, replacedValue;
+    function iteration(N, prevRow, counter) {
+        if (N > 1) {
+            nextRow = '';
+            if (counter < 6) {
+                valuesToReplace = prevRow.match(new RegExp('[0, 1]', 'g'));
+                valuesToReplace.forEach(function(value) {
+                    replacedValue = value === '0' ? '01' : '10';
+                    nextRow += replacedValue;
+                });
+            } else if (counter >= 6) {
+                valuesToReplace = prevRow.match(new RegExp('(0110100110010110)|(1001011001101001)', 'g'));
+                valuesToReplace.forEach(function(value) {
+                    replacedValue = value === '0110100110010110' ? '01101001100101101001011001101001' : '10010110011010010110100110010110';
+                    nextRow += replacedValue;
+                });
+            }   
+            return iteration(N-1, nextRow, counter + 1);        
         } else {
-            return prevRow
+            return prevRow;
         }
     }
     let firstRow = '0';
-    return iteration(N, firstRow);
+    return iteration(N, firstRow, 1);
 }
+console.log(generateRow(1));
+console.log(generateRow(2));
+console.log(generateRow(3));
+console.log(generateRow(4));
+console.log(generateRow(5));
+console.log(generateRow(6));
+console.log(generateRow(7));
+console.log(generateRow(8));
+console.log(generateRow(9));
+console.log(generateRow(10));
+// 0110 1001 pattern
+// 0110 1001 1001 0110 1001 0110 0110 1001 1001 0110 0110 1001 0110 1001 1001 0110
+// 1001 0110 0110 1001 0110 1001 1001 0110 0110 1001 1001 0110 1001 0110 0110 1001
+// 1001 0110 0110 1001 0110 1001 1001 0110 0110 1001 1001 0110 1001 0110 0110 1001 0110 1001 1001 0110 1001 0110 0110 1001 1001 0110 0110 1001 0110 1001 1001 0110
 
-
+// 0110 1001 1001 0110 1001 0110 0110 1001 1001 0110 0110 1001 0110 1001 1001 0110
+// 1001 0110 0110 1001 0110 1001 1001 0110 0110 1001 1001 0110 1001 0110 0110 1001 
+// 1001 0110 0110 1001 0110 1001 1001 0110 0110 1001 1001 0110 1001 0110 0110 1001
+// 0110 1001 1001 0110 1001 0110 0110 1001 1001 0110 0110 1001 0110 1001 1001 0110 
+// 1001 0110 0110 1001 0110 1001 1001 0110 0110 1001 1001 0110 1001 0110 0110 1001
+// 0110 1001 1001 0110 1001 0110 0110 1001 1001 0110 0110 1001 0110 1001 1001 0110
+// 0110 1001 1001 0110 1001 0110 0110 1001 1001 0110 0110 1001 0110 1001 1001 0110
+// 1001 0110 0110 1001 0110 1001 1001 0110 0110 1001 1001 0110 1001 0110 0110 1001
 function KthSymmbolInGrammar(N, K) {
     let row = generateRow(N);
     let kthSymbol = row.charAt(K-1);
