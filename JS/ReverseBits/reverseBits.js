4ec9e6b9f60704f43a205c834b55b4ec32efaffb
Vitaliy <vitaliy.cva.socio@gmail.com>
2018-10-14 21:29:43 +0600

    Start and end Reverse Bits problem

---
+++ JS/ReverseBits/reverseBits.js
@@ -0,0 +1,15 @@
+/* Description:
+
+Reverse bits of a given 32 bits unsigned integer.
+*/
+
+function reverseBits(n){
+    n = n.toString(2);
+    if (n.length < 32) {
+        n = n.padStart(32, '0');
+    }
+    return parseInt(n.split('').reverse().join(''), 2);
+}
+let answer = reverseBits(43261596);
+console.log(answer);
+  
