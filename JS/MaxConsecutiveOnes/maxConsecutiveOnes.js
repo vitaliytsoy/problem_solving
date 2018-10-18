0857db953294374a164936189d09dd9f2ca1f2c2
Vitaliy <vitaliy.cva.socio@gmail.com>
2018-10-19 00:20:04 +0600

    Finish Find Max Consecutive Ones problem

--- JS/MaxConsecutiveOnes/maxConsecutiveOnes.js
+++ JS/MaxConsecutiveOnes/maxConsecutiveOnes.js
@@ -8,12 +8,11 @@ Explanation: The first two digits or the last three digits are consecutive 1s.
     The maximum number of consecutive 1s is 3.
 */
 let findMaxConsecutiveOnes = (nums) => {
-    const countMax = 0;
-    let result = nums.reduce((prev, current) => {
-
-        return prev + current;
-    }, 0);
-
+    let match = nums.join('').match(new RegExp('1+', 'g'));
+    if (!match) return 0;
+    return match.reduce((prev, current) => {
+        return prev < current.length ? current.length : prev;
+    }, 1);
 }
-findMaxConsecutiveOnes([1,1,0,1,1,1]);
+findMaxConsecutiveOnes([1,1,1,1,1,1,1,1,1,1,10,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0]);
 
