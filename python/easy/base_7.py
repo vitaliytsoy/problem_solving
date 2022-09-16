90afbe7bc7129a084d1a3cb6a7858baef18f3e88
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2022-09-16 23:52:33 +0600

    Problem: base_7

---
+++ python/easy/base_7.py
@@ -0,0 +1,34 @@
+"""
+Given an integer num, return a string of its base 7 representation.
+
+ 
+
+Example 1:
+
+Input: num = 100
+Output: "202"
+Example 2:
+
+Input: num = -7
+Output: "-10"
+"""
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        result = ""
+
+        if num == 0:
+            return "0"
+        
+        if num < 0:
+            return "-" + self.convertToBase7(-num)
+
+        while num > 0:
+            result += str(num % 7)
+            num = int(num / 7)
+
+        return result
+
+
+solution = Solution()
+print(solution.convertToBase7(100))
+
