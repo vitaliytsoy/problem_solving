07f21efbee4a1290b264ae51d7c51eb244069876
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2022-09-30 00:07:10 +0600

    Debug code removed

--- python/easy/linked_list_reverse.py
+++ python/easy/linked_list_reverse.py
@@ -14,12 +14,14 @@ class ListNode:
         self.val = val
         self.next = next
 
-
+# 1 - 2 - 1 - 4 - 5
+# 
+# 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head == None or head.next == None:
             return head
-        
+
         last = self.reverseList(head.next)
         head.next.next = head
         head.next = None
