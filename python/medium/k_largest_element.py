c383dae7bd027c3bbd5c1e08f8dd2ba4c76e0d4c
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2022-10-03 21:56:23 +0600

    Problem: kth largest long

--- python/medium/k_largest_element.py
+++ python/medium/k_largest_element.py
@@ -36,8 +36,40 @@ class Solution:
 
         return mid[0]
 
+    def find_kth_largest(self, nums: List[int], k: int) -> int:
+        pivot = random.choice(nums)
+        left = []
+        right = []
+        mid = []
+        
+        for num in nums:
+            if num > pivot:
+                right.append(num)
+            elif num < pivot:
+                left.append(num)
+            else:
+                mid.append(num)
+
+        L, M, R = len(left), len(mid), len(right)
+        T = L + M + R
+
+        if T - L == k or (R == 0 and L == 0):
+            return mid[0]
+
+        if M > 1 and T - k in range(L, T - R):
+            return mid[0]
+
+        if T - L > k:
+            return self.findKthLargest(right, k)
+
+        if T - L < k:
+            return self.findKthLargest(left, k - (M + R))
+
 
 solution = Solution()
 # print('RESULT', solution.findKthLargest([3,2,3,1,2,4,5,5,6], 4)) # 4
 # print('RESULT', solution.findKthLargest([3,2,1,5,6,4], 2)) # 5
-print('RESULT', solution.findKthLargest([3,2,3,1,2,4,5,5,6], 4)) # 4
+# print('RESULT', solution.findKthLargest([99,99], 1)) # 99
+# print('RESULT', solution.findKthLargest([3,3,3,3,4,3,3,3,3], 5)) # 3
+# print('RESULT', solution.findKthLargest([3,2,3,1,2,4,5,5,6], 4)) # 4
+# print(set(sorted([3,2,1,5,6,4])))
