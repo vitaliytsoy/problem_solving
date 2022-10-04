65fcd0f44b53836969c16ef0d11c2a71b141b8eb
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2022-10-05 02:10:31 +0600

    Update venv

--- python/venv/bin/activate.fish
+++ python/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vitaliytsoy/Work/code/3_sandbox/problem-solving/python/venv"
+set -gx VIRTUAL_ENV "/Users/vitaliytsoy/Work/code/2_sandbox/problem_solving/python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
