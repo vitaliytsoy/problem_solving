424e0c57a9a963ba04eed91c87e77df555dd3820
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2022-10-09 19:08:33 +0600

    Problem: reverse words optimization

--- python/medium/reverse_words.py
+++ python/medium/reverse_words.py
@@ -7,8 +7,6 @@ Return a string of the words in reverse order concatenated by a single space.
 
 Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
- 
-
 Example 1:
 
 Input: s = "the sky is blue"
@@ -35,6 +33,32 @@ class Solution:
 
         return result.strip()
 
+    def reverse_words(self, s: str) -> str:
+        words = []
+        word = ''
+
+        for index, letter in enumerate(s):
+            if len(word) == 0 and letter == ' ':
+                continue
+
+            if len(word) >= 1 and letter == ' ':
+                words.append(word)
+                word = ''
+
+                continue
+            
+            word += letter
+
+            if index == len(s) - 1:
+                words.append(word)
+                word = ''
+
+        return ' '.join([words[index - 1] for index in range(len(words), 0, -1)])
+
 
 solution = Solution()
-print(solution.reverseWords("the sky   is blue"))
+# print(solution.reverseWords(" the sky   is blue  "))
+# print(solution.reverse_words(" the sky   is blue  "))
+# print(solution.reverse_words("the sky is blue"))
+# print(solution.reverse_words("a good   example"))
+print(solution.reverse_words(" asdasd df f"))
