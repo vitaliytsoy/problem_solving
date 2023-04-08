51513dd28db331bd177ae9b357d625d51e5ce0a9
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-04-08 18:47:12 +0600

    Code style

--- python/medium/course_schedule.py
+++ python/medium/course_schedule.py
@@ -31,29 +31,23 @@ class Solution:
             graph[pre_course].append(course)
             in_degree[course] += 1
             
-            # Initialize a queue with courses that have no prerequisites
         queue = deque(course for course in range(numCourses) if in_degree[course] == 0)
-        
-        print(in_degree)
-        print(queue)
-        print(graph)
-        
-           # Topological sort algorithm
+
         while queue:
             pre_course = queue.popleft()
+            
             for course in graph[pre_course]:
                 in_degree[course] -= 1
+                
                 if in_degree[course] == 0:
                     queue.append(course)
-                    
-        print(in_degree)
                     
         return all(in_degree[course] == 0 for course in range(numCourses))
         
     
 s = Solution()
-# print(s.canFinish(3, [[1,0]]))
-# print(s.canFinish(2, [[1,0],[0,1]]))
+print(s.canFinish(3, [[1,0]]))
+print(s.canFinish(2, [[1,0],[0,1]]))
 print(s.canFinish(4, [[1,0],[2,1],[3,2]]))
 
 """
