fb1c8d4af66d9fd326277654d1ccf411bd98832d
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-05-11 19:52:55 +0600

    Problem: Symmetric Tree (Recursive)

--- python/easy/symmetric_tree.py
+++ python/easy/symmetric_tree.py
@@ -11,6 +11,26 @@ from collections import deque
 
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        return self.check_nodes_symetric(root.left, root.right)
+    
+    def check_nodes_symetric(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+        if not left and not right:
+            return True
+        
+        if (left and not right) or (right and not left):
+            return False
+        
+        if left.val != right.val:
+            return False;
+        
+        return self.check_nodes_symetric(left.left, right.right) and self.check_nodes_symetric(left.right, right.left)
+        
+    # --------------
+    
+    def isSymmetricMemoryIntensive(self, root: Optional[TreeNode]) -> bool:
         left_values = self.make_tree_values(root.left)
         right_values = self.make_tree_values(root.right, True)
         
