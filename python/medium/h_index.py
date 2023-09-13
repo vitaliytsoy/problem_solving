98294f6770ff384c42eb1bd0387b1ed576b286f2
Vitaliy Tsoy <vitaliy.cva@gmail.com>
2023-09-13 18:48:56 +0600

    Problem: H Index

---
+++ python/medium/h_index.py
@@ -0,0 +1,46 @@
+"""
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+"""
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        h_index = 0
+        
+        if (len(citations) == 1):
+            return min(citations[0], 1)
+        
+        for i in range(len(citations)):
+            cit = citations[i]
+            
+            if (cit >= h_index):
+
+                if (len(citations) - (i + 1) >= cit - 1):
+                    h_index = cit
+                else:
+                    h_index = max(h_index, len(citations) - (i + 1) + 1)
+
+        return h_index
+    
+    
+s = Solution()
+print(s.hIndex([3,0,6,1,5])) # 3
+print(s.hIndex([1,3,1])) # 1
+print(s.hIndex([100])) # 1
+print(s.hIndex([11,15])) # 2
+        
