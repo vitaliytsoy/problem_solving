53c33a425b74772328c893df3643be778562919b
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-09-19 14:33:00 +0600

    Problem: Valid Sudoku

---
+++ python/medium/valid_sudoku.py
@@ -0,0 +1,108 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+"""
+from typing import List
+from collections import defaultdict
+import math
+
+class Solution:
+    def get_square_id(self, i, j) -> int:
+        return (math.ceil((i + 1) / 3) - 1) * 3 + math.ceil((j + 1) / 3)
+    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        filled = []
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                num = board[i][j]
+                
+                if (num != '.'):
+                    filled.append((i, j))
+                    
+                    
+        for (i, j) in filled:
+            num = board[i][j]
+            
+            if (num in rows[i]):
+                return False
+            
+            if (num in cols[j]):
+                return False
+            
+            if (num in squares[self.get_square_id(i, j)]):
+                return False
+            
+            rows[i].add(num)
+            cols[j].add(num)
+            squares[self.get_square_id(i, j)].add(num)
+            
+        return True
+    
+s = Solution()
+print(
+    s.isValidSudoku(
+        [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+    )
+) # True
+print(
+    s.isValidSudoku(
+        [["8","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+    )
+) # False
