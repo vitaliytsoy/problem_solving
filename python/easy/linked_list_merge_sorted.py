96d7a0f8c80296cd5f15c9cc873c5e5c44ee9dbd
Vitaliy Tsoy <vitaliy.cva@gmail.com>
2023-09-24 17:22:35 +0600

    Problem: Merge Two Sorted Lists

--- python/easy/linked_list_merge_sorted.py
+++ python/easy/linked_list_merge_sorted.py
@@ -17,6 +17,49 @@ class ListNode:
         self.next = next
 
 class Solution:
+    def get_next_node(self, node_1: Optional[ListNode], node_2: Optional[ListNode]):
+        if (node_1 == None):
+            return node_2
+        
+        if (node_2 == None):
+            return node_1
+        
+        if (node_1.val < node_2.val):
+            return node_1
+        
+        return node_2
+    
+    def merge_two_lists_duplicate(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = list1
+        p2 = list2
+        head = None
+        pointer = None
+        
+        if (list1 == None):
+            return list2
+        
+        if (list2 == None):
+            return list1
+        
+        while p1 or p2:
+            node = self.get_next_node(p1, p2)
+            
+            if head == None:
+                head = node
+                pointer = head
+            else:
+                pointer.next = node
+                pointer = pointer.next
+                
+            if p1 and node == p1:
+                p1 = p1.next
+                
+            if p2 and node == p2:
+                p2 = p2.next
+
+        return head
+    
+    
     def get_next_node(self,  node1: Optional[ListNode], node2: Optional[ListNode]):
             if node1 == None and node2 == None:
                 return None
