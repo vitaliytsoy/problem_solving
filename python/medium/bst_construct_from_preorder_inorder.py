e18489f0ad62fa21c32878db753044b0ad175cdd
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-10-03 20:08:45 +0600

    Problem: BST Construct Binary Tree from Preorder and Inorder Traversal

---
+++ python/medium/bst_construct_from_preorder_inorder.py
@@ -0,0 +1,37 @@
+"""
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+"""
+from typing import Optional, List
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # preorder - root, left, right
+        # inorder - left, root , right
+        # preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+        if inorder:
+            index = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[index])
+            root.left = self.buildTree(preorder, inorder[0:index])
+            root.right = self.buildTree(preorder, inorder[index+1:])
+    
+            return root
+
+        
