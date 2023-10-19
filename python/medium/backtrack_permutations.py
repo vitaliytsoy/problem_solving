2d3ad7fda285641568dfa945875bf686c3f20663
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-10-19 19:14:16 +0600

    Problem: Permutations

---
+++ python/medium/backtrack_permutations.py
@@ -0,0 +1,57 @@
+"""
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+
+Input: nums = [1]
+Output: [[1]] 
+"""
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if (len(nums) == 0):
+            return []
+        
+        
+        def backtrack(permutation, i, result):
+            if (i >= len(permutation)):
+                result.append(permutation.copy())
+                
+                return
+                
+            for num in nums:
+                if num in permutation:
+                    continue
+                
+                permutation[i] = num
+                
+                backtrack(permutation, i + 1, result)
+                
+            permutation[i] = None
+                
+                
+            
+        result = []
+        permutation = [None] * len(nums)
+            
+        backtrack(permutation, 0, result)
+
+        return result
+    
+s = Solution()
+
+print(s.permute([0,1])) # [[0, 1], [1, 0]]
+print(s.permute([1,2,3])) # [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+print(s.permute([1])) # [[1]]
+
