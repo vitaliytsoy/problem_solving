cb1ad573f580c57d94b5fcfe8ebd49cb8cc6d567
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-10-27 20:34:05 +0600

    Problem: Search a 2D Matrix (array)

--- python/medium/binary_search_2d_matrix.py
+++ python/medium/binary_search_2d_matrix.py
@@ -10,6 +10,27 @@ You must write a solution in O(log(m * n)) time complexity.
 from typing import List
 
 class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+        start = 0
+        end = m * n - 1
+        
+        while start <= end:
+            middle = (start + end) // 2
+            num = matrix[middle // m][middle % m]
+            
+            if target == num: 
+                return True
+            
+            if target < num:
+                end = middle - 1
+            else:
+                start = middle + 1
+        
+        return False
+    
+    
     def find_row(self, matrix, target):
         start = 0
         end = len(matrix) - 1
@@ -50,7 +71,7 @@ class Solution:
                 
         return -1
     
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    def searchMatrix1(self, matrix: List[List[int]], target: int) -> bool:
         row_index = self.find_row(matrix, target)
         
         if (row_index == -1):
@@ -63,7 +84,10 @@ class Solution:
 
     
 s = Solution()
-print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)) # true
-print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)) # false
-print(s.searchMatrix([[1]], 2)) # false
-print(s.searchMatrix([[1,3]], 3)) # true
+# print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)) # true
+# print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)) # false
+# print(s.searchMatrix([[1]], 2)) # false
+# print(s.searchMatrix([[1,3]], 3)) # true
+print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,50]], 5)) # true
+
+
