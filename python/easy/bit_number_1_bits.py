e8a85734edd1f768c661e677694ee613cc5f73c4
Vitaliy Tsoy <vitaliy.cva@gmail.com>
2023-11-09 19:51:36 +0600

    Problem: Number 1 Bits (opt)

--- python/easy/bit_number_1_bits.py
+++ python/easy/bit_number_1_bits.py
@@ -27,7 +27,7 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
         
-        for i in range(32):
+        while n:
             if (n & 1 == 1):
                 count += 1
 
