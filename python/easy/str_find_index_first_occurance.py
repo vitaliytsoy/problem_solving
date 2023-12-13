6ed607e2b42fe4257bab8a5de8a615a9571642d6
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2023-12-13 18:35:34 +0600

    Problen Find Index of First Occurance (opt)

--- python/easy/str_find_index_first_occurance.py
+++ python/easy/str_find_index_first_occurance.py
@@ -45,7 +45,21 @@ class Solution:
 
         return -1
 
-            
+        # --- 
+        
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)):
+            for j in range(len(needle) + 1):
+                if j == len(needle):
+                    return i
+                
+                if i + j == len(haystack):
+                    return -1
+
+                if needle[j] != haystack[i + j]:
+                    break
+        
+        return -1    
     
 s = Solution()
 print(s.strStr("sadbutsad", "sad")) # 0
