eeccd24ee97eecddf64c4c5d1bb6dfee28cf23f0
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2024-02-07 19:34:09 +0600

    Dead code

--- python/medium/trie_add_and_search_words.py
+++ python/medium/trie_add_and_search_words.py
@@ -77,27 +77,7 @@ class WordDictionary:
             stack.append((pointer.edges[letter], word[1:]))
             
         return False
-    
-    def search(self, word):
-        node = self.head
-        self.res = False
-        self.dfs(node, word)
-        return self.res
-    
-    def dfs(self, node, word):
-        if not word:
-            if node.is_end:
-                self.res = True
-            return 
-        if word[0] == ".":
-            for n in node.edges.values():
-                self.dfs(n, word[1:])
-        else:
-            node = node.edges.get(word[0])
-            if not node:
-                return 
-            self.dfs(node, word[1:])
-        
+
 
 # Input
 # ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
@@ -123,40 +103,3 @@ print(s.search("b.."))
 # [null,null,null,null,null,true,false,null,true,true,false,true,true,true]
 # Expected
 # [null,null,null,null,null,false,false,null,true,true,false,false,true,false]
-
-
-
-class TrieNode():
-    def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
-        self.isWord = False
-    
-class WordDictionary(object):
-    def __init__(self):
-        self.root = TrieNode()
-
-    def addWord(self, word):
-        node = self.root
-        for w in word:
-            node = node.children[w]
-        node.isWord = True
-
-    def search(self, word):
-        node = self.root
-        self.res = False
-        self.dfs(node, word)
-        return self.res
-    
-    def dfs(self, node, word):
-        if not word:
-            if node.isWord:
-                self.res = True
-            return 
-        if word[0] == ".":
-            for n in node.children.values():
-                self.dfs(n, word[1:])
-        else:
-            node = node.children.get(word[0])
-            if not node:
-                return 
-            self.dfs(node, word[1:])
