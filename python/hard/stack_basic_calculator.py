673a4775a338b17c95780c4798b4cb2017e9ee02
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2024-02-28 20:56:42 +0600

    Problem: Basic Calculator (start)

--- python/hard/stack_basic_calculator.py
+++ python/hard/stack_basic_calculator.py
@@ -18,66 +18,104 @@ Example 3:
 Input: s = "(1+(4+5+2)-3)+(6+8)"
 Output: 23
 """
-
+import re
+from collections import deque
 
 class Solution:
     operations = {'+', '-'}
     numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
     braces = {'(', ')', }
     
-    def evaluate(self, a, op, b):
-        return 0
+    def evaluate(self, expression):
+        numbers = deque(re.findall("([0-9]+)", expression))
+        operations = deque(re.findall("(\+|\-)", expression))
+        operation = '+'
+        result = 0
+        # 2-1 + 2 
+        if not operations or not numbers:
+            return 0
+        
+        if len(operations) > len(numbers) - 1:
+            operation = operations.popleft()
+        
+        
+        while numbers:
+            number = int(numbers.popleft())
+            
+            if operation == '':
+                operation = operations.popleft()
+            
+            if operation == '+':
+                result += number
+                
+            if operation == '-':
+                result -= number
+                
+            operation = ''
+            
+        return result
+    
+    def find_match_brace_index(self, expression, start):
+        braces = []
+        result = -1
+        
+        for i in range(start, len(expression)):
+            char = expression[i]
+            
+            if char not in self.braces:
+                continue
+
+            if char == '(':
+                braces.append('()')
+                continue
+                
+            if char == ')' and braces[-1] == '()':
+                result = i
+                break
+                
+        return result
     
     def calculate(self, s: str) -> int:
+        print('start', s)
         pointer = 0
-    
-    
-        num_a = 0
-        operation = '+'
-        num_b = 0
-        
-        #0 + ...s
+        expression = ''
+
+        if '(' not in s:
+            return self.evaluate(s)
+                
         while pointer < len(s):
             char = s[pointer]
             
-            if operation: 
+            if char == ' ':
+                pointer += 1
+                continue
+            
+            if char == '(': 
+                end_brace_index = self.find_match_brace_index(s, pointer)
                 
-                if char in self.number:
-                    num_b = 
-                # look for number
-                if char in self.operations or
-            else:
-                # look for operation
-            
-            
-            
-
-            # if char in self.operations and op == '':
-            #     # cases -1 -2 -    3 ...
-            #     op = char
-            #     pointer += 1
-            #     continue
-            
-            # if char in self.braces:
-            #     start = pointer
-            #     end = pointer
+                print(s, pointer, end_brace_index, s[pointer + 1:end_brace_index])
                 
-            #     while end < len(s):
-            #         if s[end] == ')':
-            #             break
-                    
-            #         end += 1
+                if end_brace_index == -1:
+                    break
                 
-            #     result += self.calculate(s[start:end])
-            #     pointer = end + 1
-            #     continue
-        
-            
+                
+                expression += str(self.calculate(s[pointer + 1:end_brace_index]))
+                pointer = end_brace_index + 1
+                
+                continue
+                
+            expression += char
             pointer += 1
-    
-        return result
+        
+        return self.evaluate(expression)
     
 s = Solution()
-print(s.calculate("1 + 1")) # 2 
-print(s.calculate(" 2-1 + 2 ")) # 3
+
+
+# print(s.evaluate(" 2-1 + 2 "))
+# print(s.calculate("1 + 1")) # 2 
+# print(s.calculate("-2 - 3")) # -5
+# print(s.calculate("-2 + 4")) # 2
+# print(s.calculate(" 2-1 + 2 ")) # 3
+
 print(s.calculate("(1+(4+5+2)-3)+(6+8)")) # 23
