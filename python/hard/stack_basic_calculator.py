78c84f44270eb9dd3db6d156b165c1bf93f2affc
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2024-03-06 20:21:27 +0600

    Problem: Basic Calculator (slow)

--- python/hard/stack_basic_calculator.py
+++ python/hard/stack_basic_calculator.py
@@ -31,13 +31,9 @@ class Solution:
         operations = deque(re.findall("(\+|\-)", expression))
         operation = '+'
         result = 0
-        # 2-1 + 2 
-        if not operations or not numbers:
-            return 0
-        
+ 
         if len(operations) > len(numbers) - 1:
             operation = operations.popleft()
-        
         
         while numbers:
             number = int(numbers.popleft())
@@ -66,17 +62,19 @@ class Solution:
                 continue
 
             if char == '(':
-                braces.append('()')
+                braces.append('(')
                 continue
                 
-            if char == ')' and braces[-1] == '()':
+            if char == ')' and braces[-1] == '(':
+                braces.pop()
+
+            if len(braces) == 0:
                 result = i
                 break
                 
         return result
     
     def calculate(self, s: str) -> int:
-        print('start', s)
         pointer = 0
         expression = ''
 
@@ -93,11 +91,8 @@ class Solution:
             if char == '(': 
                 end_brace_index = self.find_match_brace_index(s, pointer)
                 
-                print(s, pointer, end_brace_index, s[pointer + 1:end_brace_index])
-                
                 if end_brace_index == -1:
                     break
-                
                 
                 expression += str(self.calculate(s[pointer + 1:end_brace_index]))
                 pointer = end_brace_index + 1
@@ -106,6 +101,9 @@ class Solution:
                 
             expression += char
             pointer += 1
+            
+        expression = expression.replace('--', '+')
+        expression = expression.replace('+-', '-')
         
         return self.evaluate(expression)
     
@@ -113,9 +111,11 @@ s = Solution()
 
 
 # print(s.evaluate(" 2-1 + 2 "))
+# print(s.calculate("1")) # 1
 # print(s.calculate("1 + 1")) # 2 
 # print(s.calculate("-2 - 3")) # -5
 # print(s.calculate("-2 + 4")) # 2
 # print(s.calculate(" 2-1 + 2 ")) # 3
-
-print(s.calculate("(1+(4+5+2)-3)+(6+8)")) # 23
+# print(s.calculate("(1+(4+5+2)-3)+(6+8)")) # 23
+# print(s.calculate("1-(     -2)")) # 3
+# print(s.calculate("5+3-4-(1+2-7+(10-1+3+5+(3-0+(8-(3+(8-(10-(6-10-8-7+(0+0+7)-10+5-3-2+(9+0+(7+(2-(2-(9)-2+5+4+2+(2+9+1+5+5-8-9-2-9+1+0)-(5-(9)-(0-(7+9)+(10+(6-4+6))+0-2+(10+7+(8+(7-(8-(3)+(2)+(10-6+10-(2)-7-(2)+(3+(8))+(1-3-8)+6-(4+1)+(6))+6-(1)-(10+(4)+(8)+(5+(0))+(3-(6))-(9)-(4)+(2))))))-1)))+(9+6)+(0))))+3-(1))+(7))))))))")) # -35
