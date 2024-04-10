46e84e33f06bc468629b4a18ddbbdffb4dfd1f1b
Vitaliy Tsoy <vitaliy.cva.socio@gmail.com>
2024-04-10 20:05:09 +0500

    Problem: Merge k sorted lists (opt)

--- python/hard/daq_merge_k_sorted_lists.py
+++ python/hard/daq_merge_k_sorted_lists.py
@@ -27,6 +27,7 @@ Input: lists = [[]]
 Output: []
 """
 from typing import List, Optional
+import heapq
 import sys
 
 # Definition for singly-linked list.
@@ -72,6 +73,82 @@ class Solution:
             pointer.next = None
             
         return new_head.next
+    
+    # ---
+    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        new_head = ListNode()
+        pointer = new_head
+        node_list = []
+        compare_list = []
+        
+        for i in range(len(lists)):
+            node = lists[i]
+            
+            if node == None:
+                continue
+            
+            node_list.append(node)
+            
+        while node_list:
+            for index, node in enumerate(node_list):                
+                heapq.heappush(compare_list, (node.val, index))
+                
+            _, index = heapq.heappop(compare_list)
+            
+            pointer.next = node_list[index]
+            pointer = pointer.next
+            compare_list.clear()
+            
+            if node_list[index].next == None:
+                node_list.pop(index)
+            else:
+                node_list[index] = node_list[index].next
+                pointer.next = None
+            
+        return new_head.next
+    
+    #---
+    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        l = []
+        
+        for node in lists:
+            if node == None:
+                continue
+            
+            l.append(node)
+            
+            if len(l) == 2:
+                new_list = self.mergeSortedLists(l[0], l[1])
+                l.clear()
+                l.append(new_list)
+                
+        return None if not l else l[0]
+    
+    def mergeSortedLists(self, node_a, node_b):
+        new_head = ListNode()
+        pointer = new_head
+        
+        while node_a or node_b:
+            if node_b == None:
+                pointer.next = node_a
+                node_a = node_a.next
+            elif node_a == None:
+                pointer.next = node_b
+                node_b = node_b.next
+            else:
+                if node_a.val < node_b.val:
+                    pointer.next = node_a
+                    node_a = node_a.next
+                else:
+                    pointer.next = node_b
+                    node_b = node_b.next
+                
+            pointer = pointer.next
+        
+        return new_head.next
+
     
     
     
